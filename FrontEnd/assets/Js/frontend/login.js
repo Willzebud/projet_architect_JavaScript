31654624778c4e834ccdfa3253c8157a10721e2c
willyam ribiere <willyamrib@gmail.com>
2024-04-02 16:50:46 +0200

    Version 1.4

--- FrontEnd/assets/Js/frontend/login.js
+++ FrontEnd/assets/Js/frontend/login.js
@@ -1,1 +1,46 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var loginForm = document.querySelector("#login form"); // Sélectionne le formulaire dans le div #login
+
+    loginForm.addEventListener("submit", function(e) {
+        e.preventDefault(); // Empêche le comportement de soumission par défaut du formulaire
+
+        console.log("Tentative de connexion"); // Pour vérifier que l'événement submit est bien capturé
+
+        var email = document.getElementById("email").value;
+        var password = document.getElementById("password").value;
+
+        getWorks(email, password); // Passe les valeurs email et password à la fonction
+    });
+});
+
+function getWorks(email, password) {
+    console.log("Envoi des données", email, password); // Pour vérifier que les données sont bien envoyées
+
+    fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email: email, // Utilisez le paramètre email
+            password: password // Utilisez le paramètre password
+        })
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Erreur dans l\'identifiant ou le mot de passe.');
+    })
+    .then(data => {
+        console.log("Connexion réussie", data); // Confirme la réussite de la connexion
+        localStorage.setItem("token", data.token);
+        window.location.href = "index.html"; // Redirige vers la page d'accueil si la connexion est réussie
+    })
+    .catch(error => {
+        console.error("Error:", error);
+        alert(error.message); // Affiche un message d'erreur
+    });
+}
+
 
